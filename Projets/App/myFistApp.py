44f66d883d43877538e647df8e7be35bc4fb4493
Octavio Romano <octavioromano@gmail.com>
2020-12-05 12:27:13 +0100

    Création interface de base et composant graphiques

--- Projets/App/myFistApp.py
+++ Projets/App/myFistApp.py
@@ -1,10 +1,49 @@
 from PySide2 import QtWidgets
+import currency_converter
 
 class App(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.curConverter = currency_converter.CurrencyConverter()
         self.setWindowTitle("Convertisseur de devises")
+        self.setup_ui()
+        self.setup_connections()
+        self.set_default_values()
 
+    def setup_ui(self):
+        self.layout = QtWidgets.QHBoxLayout(self)
+        self.cbb_devisesFrom = QtWidgets.QComboBox()
+        self.spn_montant = QtWidgets.QSpinBox()
+        self.cbb_devisesTo = QtWidgets.QComboBox()
+        self.spn_montantConverti = QtWidgets.QSpinBox()
+        self.btn_inverser = QtWidgets.QPushButton("Inverser devises")
+
+        self.layout.addWidget(self.cbb_devisesFrom)
+        self.layout.addWidget(self.spn_montant)
+        self.layout.addWidget(self.cbb_devisesTo)
+        self.layout.addWidget(self.spn_montantConverti)
+        self.layout.addWidget(self.btn_inverser)
+
+    def set_default_values(self):
+        self.cbb_devisesFrom.addItems(sorted(self.curConverter.currencies))
+        self.cbb_devisesFrom.setCurrentText("CHF")
+
+        self.cbb_devisesTo.addItems(sorted(self.curConverter.currencies))
+        self.cbb_devisesTo.setCurrentText("EUR")
+
+        self.spn_montant.setRange(1, 1000000)
+        self.spn_montantConverti.setRange(1, 1000000)
+        self.spn_montant.setValue(1)
+        self.spn_montantConverti.setValue(1)
+
+    def setup_connections(self):
+        pass
+
+    def compute(self):
+        pass
+
+    def inverser_devises(self):
+        pass
 
 myApp = QtWidgets.QApplication([])
 win = App()
